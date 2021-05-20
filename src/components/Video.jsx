4c96d173d6de159d7be1fbf829bb3093c8306ba3
iannyip <ianyip@nullspace.com.sg>
2021-05-20 18:29:17 +0800

    added context, template code to each Video component

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,2 +1,17 @@
 import React, { useState, useContext } from "react";
+import { tiktokContext } from "../store.js";
 import styles from "./Video.module.css";
+
+import VideoFooter from "./VideoFooter.jsx";
+import VideoSidebar from "./VideoSidebar.jsx";
+
+export default function Video() {
+  const { store, dispatch } = useContext(tiktokContext);
+  return (
+    <div>
+      <h1>TIKTOK VIDEO HERE</h1>
+      <VideoFooter />
+      <VideoSidebar />
+    </div>
+  );
+}
--- src/components/VideoFooter.jsx
+++ src/components/VideoFooter.jsx
@@ -1,2 +1,12 @@
 import React, { useState, useContext } from "react";
+import { tiktokContext } from "../store.js";
 import styles from "./VideoFooter.module.css";
+
+export default function VideoFooter() {
+  const { store, dispatch } = useContext(tiktokContext);
+  return (
+    <div>
+      <h1>TIKTOK FOOTER HERE</h1>
+    </div>
+  );
+}
