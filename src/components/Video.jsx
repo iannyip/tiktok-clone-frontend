c038aa3809024cc2bf013ee1160b50bdbdd78d34
iannyip <ianyip@nullspace.com.sg>
2021-05-21 17:29:25 +0800

    added working video tag

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useContext } from "react";
+import React, { useRef, useContext, useState } from "react";
 import { tiktokContext } from "../store.js";
 import styles from "./Video.module.css";
 
@@ -9,29 +9,31 @@ export default function Video() {
   const { store, dispatch } = useContext(tiktokContext);
   const videoRef = useRef(null);
 
-  const url =
-    "https://v16-web.tiktok.com/video/tos/alisg/tos-alisg-pve-0037c001/881a1d3793e34f6796360e2a24eacb00/?a=1988&br=1914&bt=957&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&expire=1621585663&l=202105210227300102340821964900FAB4&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&policy=2&qs=0&rc=M3Zxb2VvOWtseTMzZzczM0ApOTNlZzZpOTtmNzQzNjs8OmcxYXMzMnBlbTVfLS02MTRzcy8vMmMtYzJhNjI1NV40NjE6Yw%3D%3D&signature=77d37f0848cbc2ee1eee8ee9b1517754&tk=tt_webid_v2&vl=&vr=";
-
-  const copiedlinkadd =
-    "https://v16-web.tiktok.com/video/tos/alisg/tos-alisg-pve-0037c001/881a1d3793e34f6796360e2a24eacb00/?a=1988&br=1914&bt=957&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&expire=1621594888&l=20210521050115010115004074130BF26A&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&policy=2&qs=0&rc=M3Zxb2VvOWtseTMzZzczM0ApOTNlZzZpOTtmNzQzNjs8OmcxYXMzMnBlbTVfLS02MTRzcy8vMmMtYzJhNjI1NV40NjE6Yw%3D%3D&signature=d09b1fea4b68fdefc7ef1ab39524adb1&tk=tt_webid_v2&vl=&vr=";
-
-  const onVideoPress = () => {
-    // videoRef.current;
+  // ****** added this ********
+  const [play, setPlay] = useState(false);
+  const videoPress = () => {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
   };
+  // this one can be used
+  const spookyurl =
+    "https://v16.tiktokcdn.com/5f61f7cef6a513ab259ea840e95e6a5c/60a77d07/video/tos/useast2a/tos-useast2a-pve-0068/6a9c330a632443a29f80253bf618d1b1/?a=1233&br=1434&bt=717&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202105210327220102341060152104FE16&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzRtZTpoamU7NTMzNzczM0ApNGRoZjo8OGQ0N2VpMzlpM2c0cTEzYzRkZ15gLS1kMTZzczMwMi8tNjJjMWJhMjQzL2A6Yw%3D%3D&vl=&vr=";
 
   return (
     <div className={styles.video}>
-      {/* <iframe
-        title="video"
-        ref={videoRef}
-        onClick={onVideoPress}
-        allow="autoplay"
-        src={url}
+      ​
+      <video
         className={styles.iframeVid}
-        frameborder="0"
-      ></iframe> */}
-
-      <video src={copiedlinkadd}></video>
+        loop
+        onClick={videoPress}
+        ref={videoRef}
+        src={spookyurl}
+      ></video>
       <VideoFooter />
       {/* <VideoSidebar /> */}
     </div>
