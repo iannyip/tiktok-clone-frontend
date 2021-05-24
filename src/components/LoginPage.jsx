2229042626f076706609f342c76fa8ef6c661a0c
iannyip <ianyip@nullspace.com.sg>
2021-05-24 19:45:15 +0800

    added login page logo

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -5,8 +5,12 @@ import { tiktokContext } from "../store.js";
 export default function Login() {
   const { store, dispatch } = useContext(tiktokContext);
   return (
-    <div>
+    <div className={styles.loginView}>
       <h1>This is the login page</h1>
+      <div className={styles.userLogo}>
+        <div className={styles.userLogoHead}></div>
+        <div className={styles.userLogoBody}></div>
+      </div>
     </div>
   );
 }
