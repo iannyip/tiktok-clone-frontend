f8bad1f45d955f2879a3cbbcb1b5ceddafad85f8
iannyip <ianyip@nullspace.com.sg>
2021-05-24 20:07:13 +0800

    added basic login page layout

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -6,10 +6,27 @@ export default function Login() {
   const { store, dispatch } = useContext(tiktokContext);
   return (
     <div className={styles.loginView}>
-      <h1>This is the login page</h1>
-      <div className={styles.userLogo}>
-        <div className={styles.userLogoHead}></div>
-        <div className={styles.userLogoBody}></div>
+      <div>
+        <h1>This is the login page</h1>
+        <div className={styles.userLogo}>
+          <div className={styles.userLogoHead}></div>
+          <div className={styles.userLogoBody}></div>
+        </div>
+        <div className={styles.formContainer}>
+          <input
+            className={[styles.usernameInput, styles.formStyles].join(" ")}
+            type="text"
+          />
+          <input
+            className={[styles.passwordInput, styles.formStyles].join(" ")}
+            type="password"
+          />
+          <button
+            className={[styles.submitButton, styles.formStyles].join(" ")}
+          >
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
