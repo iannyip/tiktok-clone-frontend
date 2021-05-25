b80791c36549db7faab6b3eced4d9002b6f89bb7
iannyip <ianyip@nullspace.com.sg>
2021-05-25 10:03:07 +0800

    added Router tag to FooterNavbar

--- src/components/FooterNavbar.jsx
+++ src/components/FooterNavbar.jsx
@@ -9,39 +9,41 @@ import AddBoxIcon from "@material-ui/icons/AddBox";
 import SmsOutlinedIcon from "@material-ui/icons/SmsOutlined";
 import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
 
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default function FooterNavbar() {
   const { store, dispatch } = useContext(tiktokContext);
   return (
-    <nav className={styles.FooterStyle}>
-      <ul className={styles.footerNavbar}>
-        <li>
-          <Link to="/Home">
-            <HomeIcon fontSize="default" />
-            <p>Home</p>
-          </Link>
-        </li>
-        <li>
-          <p className="icon">
-            <SearchIcon fontSize="default" />
-          </p>
-          <p>Discover</p>
-        </li>
-        <li>
-          <AddBoxIcon fontSize="default" />
-        </li>
-        <li>
-          <SmsOutlinedIcon fontSize="default" />
-          <p>Inbox</p>
-        </li>
-        <li>
-          <Link to="/Me">
-            <PersonOutlineOutlinedIcon fontSize="default" />
-            <p>Me</p>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <Router>
+      <nav className={styles.FooterStyle}>
+        <ul className={styles.footerNavbar}>
+          <li>
+            <Link to="/Home">
+              <HomeIcon fontSize="default" />
+              <p>Home</p>
+            </Link>
+          </li>
+          <li>
+            <p className="icon">
+              <SearchIcon fontSize="default" />
+            </p>
+            <p>Discover</p>
+          </li>
+          <li>
+            <AddBoxIcon fontSize="default" />
+          </li>
+          <li>
+            <SmsOutlinedIcon fontSize="default" />
+            <p>Inbox</p>
+          </li>
+          <li>
+            <Link to="/Me">
+              <PersonOutlineOutlinedIcon fontSize="default" />
+              <p>Me</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </Router>
   );
 }
