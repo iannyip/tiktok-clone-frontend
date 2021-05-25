f9e3a3fa57e4f4ce15f0f9aa1b9a0f7ad1224ffc
iannyip <ianyip@nullspace.com.sg>
2021-05-25 10:44:56 +0800

    removed unecessary imports from App.js and FooterNavbar.jsx

--- src/components/FooterNavbar.jsx
+++ src/components/FooterNavbar.jsx
@@ -1,15 +1,13 @@
 import React, { useState, useContext } from "react";
 import styles from "./FooterNavbar.module.css";
 import { tiktokContext } from "../store.js";
-import BottomNavigation from "@material-ui/core/BottomNavigation";
-import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import AddBoxIcon from "@material-ui/icons/AddBox";
 import SmsOutlinedIcon from "@material-ui/icons/SmsOutlined";
 import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
 
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function FooterNavbar() {
   const { store, dispatch } = useContext(tiktokContext);
