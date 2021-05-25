fef74c7cdb21059e7a30db8c9a5aca17ecc4b91b
iannyip <ianyip@nullspace.com.sg>
2021-05-25 16:16:54 +0800

    refactored Video, VideoSidebar, VideoFooter components to pass video data downwards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,39 @@ import styles from "./Home.module.css";
 
 export default function Home() {
   const { state, dispatch } = useContext(tiktokContext);
+  const videoObj = {
+    videourl:
+      "https://assets.mixkit.co/videos/preview/mixkit-girl-in-neon-sign-1232-large.mp4",
+    userprofileurl:
+      "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+    userId: 1,
+    username: "iannyip",
+    description: "This is my description",
+    song: "Wow this is my song!",
+    likes: 100,
+    comments: 100,
+    shares: 100,
+  };
+  const videoObj2 = {
+    videourl:
+      "https://assets.mixkit.co/videos/preview/mixkit-mysterious-pale-looking-fashion-woman-at-winter-39878-large.mp4",
+    userprofileurl:
+      "https://image.shutterstock.com/image-photo/self-portrait-beautiful-chinese-girl-260nw-1289866381.jpg",
+    userId: 2,
+    username: "penny",
+    description: "This is my description no.2",
+    song: "This is song no. 2!",
+    likes: 200,
+    comments: 200,
+    shares: 200,
+  };
+
   return (
     <div className={styles.homeVideos}>
-      <Video />
-      <Video />
-      <Video />
-      <Video />
+      <Video videoObj={videoObj} />
+      <Video videoObj={videoObj2} />
+      <Video videoObj={videoObj} />
+      <Video videoObj={videoObj2} />
     </div>
   );
 }
--- src/components/VideoSidebar.jsx
+++ src/components/VideoSidebar.jsx
@@ -1,63 +1,62 @@
 import React, { useState, useContext, useEffect } from "react";
 import { changeNumOfLikes, loadLikes, tiktokContext } from "../store.js";
 import styles from "./VideoSidebar.module.css";
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
-import ShareIcon from '@material-ui/icons/Share';
-import CommentIcon from '@material-ui/icons/Comment';
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import ShareIcon from "@material-ui/icons/Share";
+import CommentIcon from "@material-ui/icons/Comment";
 import axios from "axios";
-import cookieParser from 'cookie-parser';
-import {
-  addLike,
-  subtractLike,
-} from '../store';
+import cookieParser from "cookie-parser";
+import { addLike, subtractLike } from "../store";
 
-
-// like function works, need to set up route to display 
+// like function works, need to set up route to display
 // all videos so we can get rid of the hard coded video id
 
-export default function VideoSidebar() {
+export default function VideoSidebar({ videoObj }) {
   const { store, dispatch } = useContext(tiktokContext);
   const [liked, setLiked] = useState(false);
-  let { likes } = store;
+  let likes = videoObj.likes;
 
   useEffect(() => {
     loadLikes(dispatch);
-  }, [])
+  }, []);
 
   const handleLikeClick = () => {
-    console.log('likes +++++++', likes);
+    console.log("likes +++++++", likes);
     setLiked(true);
     likes = likes + 1;
-    console.log('updated likes', likes);
+    console.log("updated likes", likes);
     addLike(dispatch, likes);
-  }
+  };
 
   const handleUnlikeClick = () => {
     setLiked(false);
-    likes = likes - 1
+    likes = likes - 1;
     subtractLike(dispatch, likes);
-  }
+  };
 
   // TODO: image src need to be changed , hardcoded for now
   return (
     <div className={styles.videoSideBar}>
-      <img src="https://image.shutterstock.com/image-photo/self-portrait-beautiful-chinese-girl-260nw-1289866381.jpg" className={styles.rounded} />
+      <img src={videoObj.userprofileurl} className={styles.rounded} alt="..." />
       <div className={styles.videoSideBar__button}>
         {liked ? (
           <FavoriteIcon fontSize="large" onClick={handleUnlikeClick} />
         ) : (
-          <FavoriteBorderOutlinedIcon fontSize="large" onClick={handleLikeClick} />
+          <FavoriteBorderOutlinedIcon
+            fontSize="large"
+            onClick={handleLikeClick}
+          />
         )}
         <p>{likes}</p>
       </div>
       <div className={styles.videoSideBar__button}>
         <CommentIcon />
-        <p>300</p>
+        <p>{videoObj.comments}</p>
       </div>
       <div className={styles.videoSideBar__button}>
         <ShareIcon />
-        <p>300</p>
+        <p>{videoObj.shares}</p>
       </div>
     </div>
   );
