71c3d5b900304b1b17da9b9d39e887c168599798
michelle-mok <michellemokhuihui@gmail.com>
2021-05-26 22:14:00 +0800

    put videosJSX back in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,7 +58,7 @@ export default function Home() {
     <div className={styles.homeVideos}>
       {/* <Video videoObj={videoObjSample} />
       <Video videoObj={videoObjSample} /> */}
-      <ProfilePage />
+      {videosJSX}
     </div>
   );
 }
