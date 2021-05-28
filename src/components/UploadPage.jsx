63431944363b31ca230a095592443cf1acd1237d
iannyip <ianyip@nullspace.com.sg>
2021-05-28 12:43:44 +0800

    created basic layout of upload page

--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import styles from "./UploadPage.module.css";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 
 export default function UploadPage() {
-  return <div>This is the upload page</div>;
+  return (
+    <div className={styles.uploadContainer}>
+      <p className={styles.uploadText}>Please upload a video</p>
+      <div className={styles.uploadBtnContainer}>
+        <div className={styles.uploadBtn}>
+          <PhotoLibraryIcon fontSize="large" />
+        </div>
+      </div>
+    </div>
+  );
 }
