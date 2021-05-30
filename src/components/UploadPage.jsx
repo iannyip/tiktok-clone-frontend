a2c0aad8ab162312baaf832cc45fe148c461b8eb
iannyip <ianyip@nullspace.com.sg>
2021-05-30 16:06:28 +0800

    styled upload button, added event listener to upload file to firebase storage

--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./UploadPage.module.css";
 import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import { firebaseApp } from "../firebase.js";
 
 import Uppy from "@uppy/core";
 import Tus from "@uppy/tus";
@@ -28,14 +29,36 @@ export default function UploadPage() {
   });
   // UPPY CODE
 
+  const fileInputChange = (event) => {
+    // Get the file reference
+    const file = event.target.files[0];
+    // Get the firebase storage reference
+    const storageRef = firebaseApp.storage().ref();
+    // Create a new file reference on firebase storage
+    const fileRef = storageRef.child(file.name);
+    // Use this fileref to actually put the data
+    fileRef
+      .put(file)
+      .then((snapshot) => {
+        console.log(snapshot);
+        console.log("uploaded");
+        return snapshot.ref.getDownloadURL();
+      })
+      .then((result) => {
+        console.log(result);
+      });
+  };
+
   return (
     <div className={styles.uploadContainer}>
       <p className={styles.uploadText}>Please upload a video</p>
-      <FileInput uppy={uppy} pretty={true} inputName="files[]" />
+      {/* <FileInput uppy={uppy} pretty={true} inputName="files[]" /> */}
+
       <div className={styles.uploadBtnContainer}>
-        <div className={styles.uploadBtn}>
+        <label className={styles.uploadBtn}>
+          <input type="file" onChange={fileInputChange} />
           <PhotoLibraryIcon fontSize="large" />
-        </div>
+        </label>
       </div>
     </div>
   );
