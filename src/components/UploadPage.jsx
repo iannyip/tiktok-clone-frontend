f4aceef38fc3f875f6fbb188b783a37caed4959f
iannyip <ianyip@nullspace.com.sg>
2021-05-31 12:34:38 +0800

    upload form is able to upload

--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -1,38 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./UploadPage.module.css";
+import Button from "@material-ui/core/Button";
 import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import { firebaseApp } from "../firebase.js";
-
-import Uppy from "@uppy/core";
-import Tus from "@uppy/tus";
-import { DragDrop, FileInput } from "@uppy/react";
 
 export default function UploadPage() {
   const [uploadSuccess, setUploadSuccess] = useState(false);
-  // UPPY CODE
-  const uppy = new Uppy({
-    id: "Puppy",
-    meta: { type: "avatar" },
-    restrictions: { maxNumberOfFiles: 1 },
-    autoProceed: true,
-  });
-
-  uppy.use(Tus, { endpoint: "/upload" });
-
-  uppy.on("complete", (result) => {
-    const url = result.successful[0].uploadURL;
-    console.log("UPLOADED URL: ");
-    console.log(url);
-    // store.dispatch({
-    //   type: "SET_USER_AVATAR_URL",
-    //   payload: { url: url },
-    // });
-  });
-  // UPPY CODE
+  const [showForm, setShowForm] = useState(false);
+  const [description, setDescription] = useState("");
+  const [song, setSong] = useState("");
+  const inputRef = useRef(null);
 
   const fileInputChange = (event) => {
+    setShowForm(true);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handleSongChange = (event) => {
+    setSong(event.target.value);
+  };
+
+  const handlePostClick = () => {
+    console.log("about to submit");
+    // console.log(inputRef.current.files[0]);
     // Get the file reference
-    const file = event.target.files[0];
+    const file = inputRef.current.files[0];
     // Get the firebase storage reference
     const storageRef = firebaseApp.storage().ref();
     // Create a new file reference on firebase storage
@@ -54,13 +50,39 @@ export default function UploadPage() {
   return (
     <div className={styles.uploadContainer}>
       <p className={styles.uploadText}>Please upload a video</p>
-      {/* <FileInput uppy={uppy} pretty={true} inputName="files[]" /> */}
 
       <div className={styles.uploadBtnContainer}>
         <label className={styles.uploadBtn}>
-          <input type="file" onChange={fileInputChange} />
+          <input ref={inputRef} type="file" onChange={fileInputChange} />
           <PhotoLibraryIcon fontSize="large" />
         </label>
+
+        {showForm && (
+          <div className={styles.uploadForm}>
+            <input
+              type="text"
+              value={description}
+              placeholder="description"
+              onChange={handleDescriptionChange}
+            ></input>
+            <input
+              type="text"
+              value={song}
+              placeholder="song (leave blank if original audio)"
+              onChange={handleSongChange}
+            ></input>
+            <Button
+              variant="contained"
+              color="default"
+              // className={classes.button}
+              startIcon={<CloudUploadIcon />}
+              onClick={handlePostClick}
+            >
+              Upload
+            </Button>
+          </div>
+        )}
+
         {uploadSuccess && <p className={styles.success}>Upload successful</p>}
       </div>
     </div>
