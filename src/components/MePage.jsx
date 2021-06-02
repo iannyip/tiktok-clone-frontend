89fed0a34c9729d71b64d63547de64ba50f485db
iannyip <ianyip@nullspace.com.sg>
2021-06-02 14:06:02 +0800

    added comments for deep linking

--- src/components/MePage.jsx
+++ src/components/MePage.jsx
@@ -8,5 +8,10 @@ export default function MePage() {
   const { store, dispatch } = useContext(tiktokContext);
   const { isUserLoggedIn } = store;
 
+  // At the start of the app's lifecycle: useEffect(() =. {}, [])
+  // if the cookie exists
+  // make a backend req to check if user is valid
+  // if valid, backend returns info required to update state
+  //
   return <>{isUserLoggedIn ? <ProfilePage /> : <LoginPage />}</>;
 }
